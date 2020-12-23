9f54824779f5c3db6a2f6a1af939e35723cdf050
Yuqing_Chen <chenyuqing0429@foxmail.com>
2020-12-23 11:21:52 +0800

    new repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# 算法题库
+
+
+
+**Solution**文件夹内为洛谷、Codeforces等题解。（P开头为洛谷）
+
+**Template**文件夹内为板子。
---
+++ Solution/回溯/P2819.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, k, m;
+    cin >> n >> k >> m;
+    int g[n][n];
+    memset(g, 0, sizeof(g));
+    
+}
